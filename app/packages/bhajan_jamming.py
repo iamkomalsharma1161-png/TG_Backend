2a305e13929df00e47eefda8586f47301eec1763
theks2502 <theks2502@gmail.com>
2026-03-23 19:10:52 +0530

    Change odt mail

--- app/packages/bhajan_jamming.py
+++ app/packages/bhajan_jamming.py
@@ -41,8 +41,8 @@ async def send_bhajan_jamming_email(data):
 
     Event Details
     📅 Date: 29th March 2026
-    ⏰ Time: [Event Time]
-    📍 Venue: [Cafe Name & Location]
+    ⏰ Time: 6:30 PM onwards
+    📍 Venue: Oh!Waah Momo & Waffle , Siddharth Lake City , Bhopal
     🎟 Entry: Free & Open for All
 
     Feel free to come with friends and be part of this beautiful evening.
