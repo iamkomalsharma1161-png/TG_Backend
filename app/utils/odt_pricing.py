55326e5b216d3b3548e4bea9893e414589170117
theks2502 <theks2502@gmail.com>
2026-04-06 09:45:08 +0530

    odt price changes

--- app/utils/odt_pricing.py
+++ app/utils/odt_pricing.py
@@ -1,7 +1,9 @@
 def get_price_per_person(total_people: int , meal_preference :bool) :
     if meal_preference == True :
-        if total_people <= 3:
+        if total_people = 1:
             return 1351
+        elif total_people <= 3:
+            return 1301
         elif total_people <= 5:
             return 1275
         else:
